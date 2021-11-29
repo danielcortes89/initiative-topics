93a9771a8a2099b76eb66a86d6efa1edb45b568e
unknown <daniel.cortes.83944@gmail.com>
2021-11-29 09:15:07 -0600

    feat: Added basic model

--- src/App.js
+++ src/App.js
@@ -2,11 +2,31 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+    return (
+        <div className="App">
+        	<div className="single-unit">
+				<h3>The Subject</h3>
+				<span>Status: Planned</span>
+				<p>
+					Current Order: 1
+				</p>
+      		</div>
+			  <div className="single-unit">
+				<h3>The Subject</h3>
+				<span>Status: Planned</span>
+				<p>
+					Current Order: 1
+				</p>
+      		</div>
+			  <div className="single-unit">
+				<h3>The Subject</h3>
+				<span>Status: Planned</span>
+				<p>
+					Current Order: 1
+				</p>
+      		</div>
+    	</div>
+  	);
 }
 
 export default App;
